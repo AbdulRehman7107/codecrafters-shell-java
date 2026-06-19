78c695ce9cb1d45899529cdaf9c716baca1a6326
AbdulRehman7107 <mullaabdulrehman57@gmail.com>
2026-06-19 21:04:22 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,7 +3,6 @@ import java.io.File;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        // TODO: Uncomment the code below to pass the first stage
     	Scanner sc = new Scanner(System.in);
     	List<String> builtins = List.of("echo", "exit", "type", "pwd", "cd");
     	
@@ -16,159 +15,149 @@ public class Main {
     			continue;
     		}
     		
-    		//Parse the input string into arguments properly handling single quotes
+    		// Parse the input string into arguments properly handling quotes and backslashes
     		List<String> argsList = parseArgument(string);
-    		if(string.isEmpty()) {
+    		if(argsList.isEmpty()) {
     			continue;
     		}
     		
     		String command = argsList.get(0);
     		
-    		//handle exit command
-            if(string.equals("exit")) {
+    		// 1. Handle exit command (Using parsed variable)
+            if(command.equals("exit")) {
             	break;
             }
             
-            //handle echo command
-            else if(string.startsWith("echo")) {
+            // 2. Handle echo command (Using parsed variable)
+            else if(command.equals("echo")) {
             	StringBuilder sb = new StringBuilder();
-            	for(int i=1; i< argsList.size();i++) {
+            	for(int i = 1; i < argsList.size(); i++) {
             		sb.append(argsList.get(i));
-            		if(i< argsList.size()-1) {
+            		if(i < argsList.size() - 1) {
             			sb.append(" ");
             		}
             	}
             	System.out.println(sb.toString());
             }
             
-            //handle pwd command
-            else if (string.equals("pwd")) {
-               
+            // 3. Handle pwd command (Using parsed variable)
+            else if (command.equals("pwd")) {
                 String currentDir = System.getProperty("user.dir");
                 System.out.println(currentDir);
             }
             
-            //handle cd command
-            else if(string.startsWith("cd")) {
-            	String pathArg = argsList.size() > 1? argsList.get(1) : "";
+            // 4. Handle cd command (Using parsed variable)
+            else if(command.equals("cd")) {
+            	String pathArg = argsList.size() > 1 ? argsList.get(1) : "";
             	File newDir;
             	
             	if(pathArg.startsWith("/")) {
             		newDir = new File(pathArg);
             	} 
-            	
             	else if(pathArg.equals("~")) {
             		String homeDir = System.getenv("HOME");
             		newDir = new File(homeDir);
             	}
-            	
             	else {
-            		newDir = new File(System.getProperty("user.dir"),pathArg);
-            		
+            		newDir = new File(System.getProperty("user.dir"), pathArg);
             	}
             	
             	newDir = newDir.getAbsoluteFile();
             	
             	try {
             		newDir = newDir.getCanonicalFile();
-            	}catch(Exception a) {
-            		
-            	}
+            	} catch(Exception a) {}
             	
             	if(newDir.exists() && newDir.isDirectory()) {
             		System.setProperty("user.dir", newDir.getAbsolutePath());
             	} else {
-            		System.out.println("cd: "+pathArg+": No such file or directory");
+            		System.out.println("cd: " + pathArg + ": No such file or directory");
             		System.out.flush();
             	}
             }
             
-            // handle type command
-            else if(string.startsWith("type")) {
-            	String arg = argsList.size()> 1? argsList.get(1):"";
+            // 5. Handle type command (Using parsed variable)
+            else if(command.equals("type")) {
+            	String arg = argsList.size() > 1 ? argsList.get(1) : "";
             	
             	if(builtins.contains(arg)) {
-            		System.out.println(arg+" is a shell builtin");
+            		System.out.println(arg + " is a shell builtin");
             	} else {
             		String pathRoute = getPath(arg);
             		if(pathRoute != null) {
-            			System.out.println(arg+" is "+pathRoute);
-            		}else {
-                		System.out.println(arg+": not found");
+            			System.out.println(arg + " is " + pathRoute);
+            		} else {
+                		System.out.println(arg + ": not found");
                 	}
             	}
             }
             
-            //handle external programs
+            // 6. Handle external programs
             else {
-//            	String[] inputParts = string.split(" ");
-//            	String command = inputParts[0];
-            	
             	String executablePath = getPath(command);
-            	
             	if(executablePath != null) {
-//            		List<String> commandList = new ArrayList<>();
-//            		commandList.add(command);
-//            		
-//            		for(int i=1; i<inputParts.length; i++) {
-//            			commandList.add(inputParts[i]);
-//            		}
-            		
-            		ProcessBuilder pb =new ProcessBuilder(argsList);
+            		ProcessBuilder pb = new ProcessBuilder(argsList);
             		pb.directory(new File(System.getProperty("user.dir")));
             		pb.inheritIO();
             		
             		Process process = pb.start();
             		process.waitFor();
             	} else {
-            		System.out.println(string+": command not found");
+            		System.out.println(command + ": command not found");
             	}
             }
     	}  
     }
     
-    //helper method to parse strings supporting single quotes and concatenation
-    private static List<String> parseArgument(String string){
+    // Updated helper method to support backslash escaping outside quotes
+    private static List<String> parseArgument(String string) {
     	List<String> list = new ArrayList<>();
     	StringBuilder currentArg = new StringBuilder();
     	boolean inSingleQuotes = false;
     	boolean inDoubleQuotes = false;
-    	boolean hasContent = false; //tracks if we have started building an argument
+    	boolean hasContent = false; 
     	
-    	for(int i=0; i< string.length();i++) {
+    	for(int i = 0; i < string.length(); i++) {
     		char c = string.charAt(i);
     		
     		if(inSingleQuotes) {
-    			if(c== '\'') {
-    				inSingleQuotes = false; //close quote
-    			}
-    			else {
+    			if(c == '\'') {
+    				inSingleQuotes = false; 
+    			} else {
     				currentArg.append(c);
     			}
     			hasContent = true;
     		}
     		else if(inDoubleQuotes) {
     			if(c == '"') {
-    				inDoubleQuotes = false;//close double
-    			}else {
+    				inDoubleQuotes = false;
+    			} else {
     				currentArg.append(c);
     			}
     			hasContent = true;
     		}
     		else {
-    			if(c == '\'') {
-    				inSingleQuotes = true;//open quote
-    				hasContent=true;
+    			// NEW: Handle backslash escaping outside of quotes
+    			if(c == '\\') {
+    				if(i + 1 < string.length()) {
+    					currentArg.append(string.charAt(i + 1)); // Append the escaped next char literally
+    					i++; // Skip the next character as it's processed
+    					hasContent = true;
+    				}
     			}
-    			else if(c== '"') {
+    			else if(c == '\'') {
+    				inSingleQuotes = true;
+    				hasContent = true;
+    			}
+    			else if(c == '"') {
     				inDoubleQuotes = true;
-    				hasContent= true;
+    				hasContent = true;
     			}
     			else if(c == ' ') {
     				if(hasContent) {
     					list.add(currentArg.toString());
     					currentArg.setLength(0);
-    					hasContent= false;
+    					hasContent = false;
     				}
     			} else {
     				currentArg.append(c);
@@ -190,7 +179,6 @@ public class Main {
     	}
     	
     	String[] directories = pathEnv.split(File.pathSeparator);
-    	
     	for(String directory : directories) {
     		File file = new File(directory, command);
     		if(file.exists() && file.canExecute()) {
